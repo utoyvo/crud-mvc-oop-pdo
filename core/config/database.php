c9ff47bcb78804ae20700cd422b399a140937d53
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-15 14:04:03 +0200

    Create database.php

---
+++ core/config/database.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Database config
+ *
+ * @package CRUD MVC OOP PDO
+ * @link    https://github.com/utoyvo/crud-mvc-oop-pdo/blob/master/core/config/database.php
+ */
+
+$this->config['db'] = array(
+	'driver'   => 'mysql',
+	'host'     => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'name'     => 'crud-mvc-oop-pdo'
+);
