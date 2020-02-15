7ee0642ba5566c5c93e236d91318353d3259c49c
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-15 14:04:26 +0200

    Create session.php

---
+++ core/config/session.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Session config
+ *
+ * @package CRUD MVC OOP PDO
+ * @link    https://github.com/utoyvo/crud-mvc-oop-pdo/blob/master/core/config/session.php
+ */
+
+$this->config['session-name'] = 'SID';
