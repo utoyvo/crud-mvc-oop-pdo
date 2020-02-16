34ac706e9a454649e8d4015ed9ad1d387bdc01c1
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-17 01:17:34 +0200

    Create header.php

---
+++ app/views/header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Header
+ *
+ * @package CRUD MVC OOP PDO
+ * @link    https://github.com/utoyvo/crud-mvc-oop-pdo/blob/master/app/views/header.php
+ */
+
+?><!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!--[if lt IE 9]>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<![endif]-->
+
+	<meta http-equiv="x-dns-prefetch-control" content="on">
+	<link rel="dns-prefetch" href="//stackpath.bootstrapcdn.com">
+	<link rel="dns-prefetch" href="//code.jquery.com">
+	<link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
+
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/public/assets/css/style.css">
+
+	<title><?= Site::title( $title ); ?></title>
+	<meta name="description" content="A simple and intuitive CRUD system using the MVC pattern in OOP paradigm. To connect to the database using PDO.">
+	<meta name="keywords" content="crud, mvc, oop, pdo, php, sql, js, jquery, html, css bootstrap">
+</head>
+
+<body>
+
+<div class="container">
