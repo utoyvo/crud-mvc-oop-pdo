21cf09a233985e3951c0fed94907a8b124a8d331
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-17 01:18:41 +0200

    Create footer.php

---
+++ app/views/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Footer
+ *
+ * @package CRUD MVC OOP PDO
+ * @link    https://github.com/utoyvo/crud-mvc-oop-pdo/blob/master/app/views/footer.php
+ */
+
+?>
+
+	<footer class="text-center">
+		<p>
+			<a href="https://github.com/utoyvo/crud-mvc-oop-pdo" target="_blank">https://github.com/utoyvo/crud-mvc-oop-pdo</a>
+			<br>With <span style="color:#f00">&#60;&#51;</span> by <a href="https://github.com/utoyvo" target="_blank">@utoyvo</a>
+			<br>v<?= Site::version(); ?>
+		</p>
+	</footer>
+</div><!-- .container -->
+
+<!-- scripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="/public/assets/js/main.js"></script>
+
+</body></html>
