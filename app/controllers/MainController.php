f08b2787173c2581620ae7ef1f334987903a9aa1
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-17 01:13:20 +0200

    Create MainController.php

---
+++ app/controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main Controller
+ *
+ * @package CRUD MVC OOP PDO
+ * @link    https://github.com/utoyvo/crud-mvc-oop-pdo/blob/master/app/controllers/MainController.php
+ */
+
+class MainController extends Controller
+{
+
+	/**
+	 * Index
+	 *
+	 * http://localhost/
+	 */
+	public function index()
+	{
+	}
+
+}
