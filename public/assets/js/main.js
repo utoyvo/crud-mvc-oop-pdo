0ffb5a0a8a5f43209fa5d1d630c5e5a8b2deb6db
Oleksandr Klochko <utoyvo@protonmail.com>
2020-02-19 23:27:55 +0200

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,4 +7,37 @@
 
 jQuery( function () {
 
+	/**
+	 * Upload File
+	 */
+
+	/*
+	 * Real URL
+	 */
+	function readURL( input ) {
+		if ( input.files && input.files[0] ) {
+			var reader = new FileReader();
+			reader.onload = function( e ) {
+				$( '#prev' ).attr( 'src', e.target.result );
+			}
+
+			reader.readAsDataURL( input.files[0] );
+		}
+	}
+
+	// Prev
+	$( '#post-cover' ).change( function() {
+		readURL( this );
+	} );
+
+	// Reset
+	$( '#post-cover-reset' ).on( 'click', function() {
+		$( '#post-cover' ).val( '' );
+		$( '#prev' ).attr( 'src', '' );
+	} );
+
+	/**
+	 * Tolltip
+	 */
+	$( '[data-toggle="tooltip"]' ).tooltip();
 } );
